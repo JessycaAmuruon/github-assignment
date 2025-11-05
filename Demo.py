bfc372a3544dc61da2468d193e4df7aab8517fc9
Jessyca Amuruon <157784060+JessycaAmuruon@users.noreply.github.com>
2025-11-05 16:39:10 +0300

    demo python file
    A short demo for Python lab exercises.

---
+++ Demo.py
@@ -0,0 +1,48 @@
+#test labs by Kasey Nguyen#
+'''name = 'Jessyca' 
+age = 20 
+country = 'Kenya'
+#using variable to print
+print("name:",name,"age:",age,"country:",country, )
+#using f-string to print
+print(f"My name is {name} and I am of age {age} from the country {country}") '''
+
+#LAB 1 EX 2
+'''print("My name is Jessyca Amuruon") '''
+
+#LAB 1 EX 3
+#python program that contains name, age and major
+'''myname = "Jessyca" 
+myage = 21
+mymajor = "BISF"
+
+print("My name is", myname)
+print("My age is", myage)
+print("My major is", mymajor) '''
+
+#LAB 1 EX 4
+'''user_name = input("Enter your name:")
+user_age = input("Enter your age:")
+print(user_age)
+print(user_name) '''
+
+#LAB 1 EX 5
+'''user_height = float(input("Enter your height in feet and inches: "))
+
+user_height *= 12 #conversion to inches
+user_height = round(user_height, 2) #rounding the converted value to 2 decimal 
+print("You are", user_height, "inches")
+
+user_height *= 0.0254 #conversion to meters
+user_height = round(user_height, 2)
+print("You are", user_height, "meters") '''
+
+#LAB 1 EX 7
+'''customer_storage = int(input("Enter storage need in GB: "))
+monthly_cost = 100.00
+addition_storage = customer_storage - 750
+addition_fee = addition_storage * 5.00
+total_cost = addition_fee + monthly_cost
+print("Monthly cost and additional cost is: ", total_cost)'''
+
+#LAB 1 EX 
